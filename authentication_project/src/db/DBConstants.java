d6a57b943e225953c78daa11c76955b725acdbb7
Musashi Sakamoto <1292602b@gmail.com>
2020-08-05 20:09:00 -0700

    constants made to stubs

--- authentication_project/src/db/DBConstants.java
+++ authentication_project/src/db/DBConstants.java
@@ -1,7 +1,7 @@
 package db;
 
 public class DBConstants {
-	public static final String USERNAME = "root";
-	public static final String PASSWORD = "password";
+	public static final String USERNAME = "YOUR_USERNAME_HERE";
+	public static final String PASSWORD = "YOUR_PASSWORD_HERE";
 	public static final String CONN_STRING = "jdbc:mysql://localhost:3306/java_project?serverTimezone=UTC";
 }
